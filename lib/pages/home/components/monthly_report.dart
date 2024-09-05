20b597cd2ab9a975f2190554ab892b5630b6bdb5
Jacopo De Gattis <jacopo.degattis@gmail.com>
2024-09-05 14:01:51 +0200

    feat: add internal layout for monthly report section along with graphs

--- lib/pages/home/components/monthly_report.dart
+++ lib/pages/home/components/monthly_report.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:makeitcount/pages/home/components/pie_chart.dart';
 
 class MonthlyReport extends StatefulWidget {
   const MonthlyReport({super.key});
@@ -18,9 +19,48 @@ class _MonthlyReportState extends State<MonthlyReport> {
             margin: EdgeInsets.only(top: 15, bottom: 10),
             child: Text("Monthly report",
                 style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold))),
-        Card(
-          child: Container(
-              width: double.infinity, height: 150, child: Text("Hello World")),
+        const Card(
+          child: Padding(
+            padding: EdgeInsets.only(
+                left: 15.0, right: 15.0, top: 15.0, bottom: 15.0),
+            child: SizedBox(
+                width: double.infinity,
+                height: 180,
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Row(
+                      children: [
+                        ExpensesPieChart(
+                            header: "Incomes",
+                            progressColor: Color(0xFF1572A1),
+                            percent: 0.74,
+                            footer: "4,586.89"),
+                        SizedBox(
+                          width: 20.0,
+                        ),
+                        ExpensesPieChart(
+                          header: "Expenses",
+                          progressColor: Color(0xFFC84361),
+                          percent: 0.26,
+                          footer: "300.99",
+                        ),
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Total",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        Text("\$ 4,285.90")
+                      ],
+                    )
+                  ],
+                )),
+          ),
         )
       ],
     );
