1fca8d4dc64187e11bdca184ee908e085967ab39
Jacopo De Gattis <jacopo.degattis@gmail.com>
2024-09-06 21:16:40 +0200

    feat: add debounce time between each tab scroll along with spinner fix: fix some wrong proportions of widgets on different displays chore: improve performance moving "movements" fetch to specific component instead of "Home" feat: tab startup index euivalent to current month

--- lib/pages/home/components/expenses_preview.dart
+++ lib/pages/home/components/expenses_preview.dart
@@ -1,29 +1,57 @@
 import 'package:flutter/material.dart';
-import 'package:makeitcount/state/models/movement.model.dart';
 import 'package:makeitcount/pages/home/components/monthly_report.dart';
 import 'package:makeitcount/pages/home/components/movements.dart';
+import 'package:makeitcount/util.dart';
 
 // TODO: probably improve this name
 // This could simply aggregate both 'Monthly report'
 // and 'Movements' components
 class ExpensesPreview extends StatefulWidget {
-  final List<MovementModel> movements;
+  // final List<MovementModel> movements;
+  final int month;
 
-  const ExpensesPreview({super.key, required this.movements});
+  const ExpensesPreview({super.key, required this.month});
 
   @override
   State<ExpensesPreview> createState() => _ExpensesPreviewState();
 }
 
 class _ExpensesPreviewState extends State<ExpensesPreview> {
+  bool isDebouncing = true;
+  final _debouncer = Debouncer(milliseconds: 1000);
+
+  @override
+  void initState() {
+    super.initState();
+    _debouncer.run(() {
+      setState(() {
+        isDebouncing = false;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _debouncer.stop();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const MonthlyReport(),
-          Movements(movements: widget.movements)
-        ]);
+    return !isDebouncing
+        ? Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+                const MonthlyReport(),
+                Movements(
+                  month: widget.month,
+                )
+              ])
+        : const Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [CircularProgressIndicator()],
+          );
   }
 }
--- lib/util.dart
+++ lib/util.dart
@@ -1,10 +1,13 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 TextTheme createTextTheme(
     BuildContext context, String bodyFontString, String displayFontString) {
   TextTheme baseTextTheme = Theme.of(context).textTheme;
-  TextTheme bodyTextTheme = GoogleFonts.getTextTheme(bodyFontString, baseTextTheme);
+  TextTheme bodyTextTheme =
+      GoogleFonts.getTextTheme(bodyFontString, baseTextTheme);
   TextTheme displayTextTheme =
       GoogleFonts.getTextTheme(displayFontString, baseTextTheme);
   TextTheme textTheme = displayTextTheme.copyWith(
@@ -16,4 +19,21 @@ TextTheme createTextTheme(
     labelSmall: bodyTextTheme.labelSmall,
   );
   return textTheme;
+}
+
+// Source: https://medium.com/codingmountain-blog/debouncing-in-flutter-ed74847e17ff
+class Debouncer {
+  final int milliseconds;
+  Timer? _timer;
+  Debouncer({required this.milliseconds});
+  void run(VoidCallback action) {
+    if (_timer != null) {
+      _timer!.cancel();
+    }
+    _timer = Timer(Duration(milliseconds: milliseconds), action);
+  }
+
+  void stop() {
+    _timer!.cancel();
+  }
 }
